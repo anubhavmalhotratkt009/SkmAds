b8c71f72fc8da552d873346e7c82ff211fdf254a
Anubhav <anubhav@tekiestech.com>
2024-01-29 18:48:26 +0530

    Updates

---
+++ assets/HomeRoutes-Ypp8PYG8.js
@@ -0,0 +1,7 @@
+import{r as t,_,j as o}from"./index-1PIaFaHs.js";const r=t.lazy(()=>_(()=>import("./HeroContainer-Vsd0-pJW.js"),__vite__mapDeps([0,1,2]))),e=t.lazy(()=>_(()=>import("./AboutHome-t7WehfO5.js"),__vite__mapDeps([3,1,2]))),s=t.lazy(()=>_(()=>import("./SolutionsHome-ew17_ua-.js"),__vite__mapDeps([4,1,2]))),i=t.lazy(()=>_(()=>import("./ServicesHome-JmKRw1cd.js"),__vite__mapDeps([5,1,2]))),a=t.lazy(()=>_(()=>import("./TrackingPartnersHome-rQdY1NJb.js"),__vite__mapDeps([6,1,2]))),m=t.lazy(()=>_(()=>import("./TestimonialsHome-21mrBtgP.js"),__vite__mapDeps([7,1,2]))),E=()=>o.jsxs(o.Fragment,{children:[o.jsx(r,{}),o.jsx(e,{}),o.jsx(s,{}),o.jsx(i,{}),o.jsx(a,{}),o.jsx(m,{})]});export{E as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/HeroContainer-Vsd0-pJW.js","assets/index-1PIaFaHs.js","assets/index-UwE4F8Sc.css","assets/AboutHome-t7WehfO5.js","assets/SolutionsHome-ew17_ua-.js","assets/ServicesHome-JmKRw1cd.js","assets/TrackingPartnersHome-rQdY1NJb.js","assets/TestimonialsHome-21mrBtgP.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
