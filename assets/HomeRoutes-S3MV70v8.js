439b6ce273b8a32d0d4cf044eb3aeccc30d3c681
Anubhav <anubhav@tekiestech.com>
2024-02-02 15:53:45 +0530

    Updates

--- assets/HomeRoutes-S3MV70v8.js
+++
@@ -1,7 +0,0 @@
-import{r as t,_,j as o}from"./index-1Wu0ies5.js";const r=t.lazy(()=>_(()=>import("./HeroContainer-Qu-FQfGo.js"),__vite__mapDeps([0,1,2,3]))),e=t.lazy(()=>_(()=>import("./AboutHome-sSfs-C1P.js"),__vite__mapDeps([4,1,2,3]))),s=t.lazy(()=>_(()=>import("./SolutionsHome-WYD5VfcN.js"),__vite__mapDeps([5,1,2,3]))),i=t.lazy(()=>_(()=>import("./ServicesHome-aLn5PdXe.js"),__vite__mapDeps([6,1,2]))),a=t.lazy(()=>_(()=>import("./TrackingPartnersHome-H3Uun4lj.js"),__vite__mapDeps([7,1,2,3]))),m=t.lazy(()=>_(()=>import("./TestimonialsHome-QSyLPV0b.js"),__vite__mapDeps([8,1,2]))),E=()=>o.jsxs(o.Fragment,{children:[o.jsx(r,{}),o.jsx(e,{}),o.jsx(s,{}),o.jsx(i,{}),o.jsx(a,{}),o.jsx(m,{})]});export{E as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/HeroContainer-Qu-FQfGo.js","assets/index-1Wu0ies5.js","assets/index-iaUfogQi.css","assets/motion-bTcd8qDx.js","assets/AboutHome-sSfs-C1P.js","assets/SolutionsHome-WYD5VfcN.js","assets/ServicesHome-aLn5PdXe.js","assets/TrackingPartnersHome-H3Uun4lj.js","assets/TestimonialsHome-QSyLPV0b.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
---
+++ assets/HomeRoutes-g78UgxLQ.js
@@ -0,0 +1,7 @@
+import{r as t,_,j as o}from"./index-MgZ3xjgN.js";const r=t.lazy(()=>_(()=>import("./HeroContainer-ySk0pvsA.js"),__vite__mapDeps([0,1,2,3]))),e=t.lazy(()=>_(()=>import("./AboutHome-URRfk5fU.js"),__vite__mapDeps([4,1,2,3]))),s=t.lazy(()=>_(()=>import("./SolutionsHome-D4Jzb4sD.js"),__vite__mapDeps([5,1,2,3]))),i=t.lazy(()=>_(()=>import("./ServicesHome-U46BaUhV.js"),__vite__mapDeps([6,1,2]))),a=t.lazy(()=>_(()=>import("./TrackingPartnersHome-op-ju4o2.js"),__vite__mapDeps([7,1,2,3]))),m=t.lazy(()=>_(()=>import("./TestimonialsHome-d9EScLza.js"),__vite__mapDeps([8,1,2]))),E=()=>o.jsxs(o.Fragment,{children:[o.jsx(r,{}),o.jsx(e,{}),o.jsx(s,{}),o.jsx(i,{}),o.jsx(a,{}),o.jsx(m,{})]});export{E as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/HeroContainer-ySk0pvsA.js","assets/index-MgZ3xjgN.js","assets/index-ud4Q6AuT.css","assets/motion-hwQ8_2fr.js","assets/AboutHome-URRfk5fU.js","assets/SolutionsHome-D4Jzb4sD.js","assets/ServicesHome-U46BaUhV.js","assets/TrackingPartnersHome-op-ju4o2.js","assets/TestimonialsHome-d9EScLza.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
