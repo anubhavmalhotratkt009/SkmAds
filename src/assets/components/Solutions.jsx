be36236c0695511e8f8c43bc4822c1c0c2b66f95
Anubhav <anubhav@tekiestech.com>
2024-02-15 14:27:26 +0530

    new

--- src/assets/components/Solutions.jsx
+++ src/assets/components/Solutions.jsx
@@ -11,30 +11,29 @@ const aboutTypes = [
   {
     header: "SKMADS Mobile",
     title:
-      "At SKMADS, our Mobile Advertising solutions are designed to help app developers acquire high-quality, engaged users.",
-    desc: `At SKMADS, our Mobile Advertising solutions are designed to help app developers acquire high-quality, engaged users. We use our proprietary tracking technology and trusted third-party partners to run your campaigns on our network of thousands of traffic sources, all optimized in real-time to meet your KPIs. Our experienced team specializes in in-app and mobile web advertising, and we work with a variety of platforms to ensure maximum reach and impact.
+      "SKMADS has its  Mobile Advertising solutions that are designed to help app developers acquire high-quality, engaged users.",
+    desc: `  We use our proprietary tracking technology and trusted third-party partners to run your campaigns on our network of thousands of traffic sources, all optimized in real-time to meet your KPIs. Our experienced team specializes in in-app and mobile web advertising, and we work with a variety of platforms to ensure maximum reach and impact.
     We offer customized mobile advertising campaigns tailored to your business needs, helping you to reach and engage your target audience on their mobile devices.`,
-    img: "t1.jpg",
+    img: "t-1.jpg",
+    left: true,
   },
   {
     header: "SKMADS Convert",
     title:
-      "SKMADS'Performance-Based Advertising drives lead acquisition and customer conversion, fostering loyalty and expanding brands' reach.",
-    desc: `At SKMADS, our Performance-Based Advertising solutions are designed to help brands acquire new qualified leads and convert prospects into loyal customers.
+      "SKMADS 'Performance-Based Advertising drives lead acquisition and customer conversion, fostering loyalty and expanding brands' reach.",
+    desc: `SKMADS, our Performance-Based Advertising solutions are designed to help brands acquire new qualified leads and convert prospects into loyal customers.
 
     Our team specializes in developing customized cross-channel campaigns that reach your customers on the media they're consuming, leveraging our extended network of partners to deliver targeted, effective advertising. We use advanced tracking and optimization tools to ensure that your campaigns are achieving the desired results, whether that's increased leads, sales, or other performance metrics. With SKM Ads, you can trust that your advertising dollars are being used effectively and efficiently to drive real results for your business.`,
-    img: "t3.jpg",
+    img: "t-3.jpg",
+    left: false,
   },
   {
     header: "SKMADS Targeting",
     title:
       "SKMADS' programmatic ad solutions leverage Target's network of SSPs and DSPs for extensive web and mobile inventory access.",
-    desc: `SKMADS programmatic advertising solutions are powered by our Target business unit. With our extensive network of supply-side platforms (SSPs) and demand-side platforms (DSPs), we can access a vast inventory of advertising opportunities across the web and mobile apps.
-
-    Our programmatic buying process allows us to add layers of data to your campaigns, ensuring precise targeting or re-targeting of specific audiences. Simply set your bid and targeting criteria, and we'll handle the rest - running your campaign across millions of users while optimizing against your KPIs.
-    
-    Our data-driven approach empowers us to maximize the success of your campaigns, with real-time optimization based on insights from ongoing performance analysis. Whether you're looking to drive brand awareness, increase website traffic, or boost sales, SKMADS programmatic solutions can help you achieve your goals with unparalleled efficiency and accuracy.`,
-    img: "t2.jpg",
+    desc: `SKMADS programmatic advertising, powered by our Target unit, offers comprehensive solutions with access to a vast network of SSPs and DSPs for web and mobile ad inventory. Our data-driven approach enables precise audience targeting, real-time optimization, and ongoing performance analysis, ensuring maximum campaign success across millions of users. Whether you aim to drive brand awareness, increase website traffic, or boost sales, SKMADS delivers unparalleled efficiency and accuracy, empowering you to achieve your advertising goals effectively.`,
+    img: "t-2.jpg",
+    left: true,
   },
 ];
 
@@ -178,88 +177,82 @@ const Solutions = () => {
       </div>
 
       <div
-        className="d-flex flex-row py-5 gap-5"
+        className="d-flex flex-row pt-5 gap-5"
         style={{ background: "linear-gradient(to bottom,  white,#fff3fc)" }}
       >
-        <div className="col-lg-6">
-          <div className="w-100">
-            <img src="./images/a-1.jpg" style={{ position: "sticky" }} />
-          </div>
-          <div>
-            <img src="https://hubhtml.liquid-themes.com/assets/images/demo/start-hub-8/over-vision-2.jpg" />
-          </div>
-          <div>
-            <img src="https://hubhtml.liquid-themes.com/assets/images/demo/start-hub-8/over-vision-1.jpg" />
-          </div>
-        </div>
-        <div className="col-lg-5 d-flex justify-content-center flex-column">
-          <div className="py-5 px-3 align-items-center text-start">
-            {aboutTypes.map((ab) => {
-              return (
-                <div className="d-flex flex-wrap ca-initvalues-applied lqd-animations-done snipcss-5mnKy">
-                  <div className="d-flex flex-wrap pb-200 module-content">
-                    <div className="m-0  rounded-1 bg-slate-200 ld-fancy-heading position-relative animation-element lqd-unit-animation-done">
-                      <h6 className="ld-fh-element d-inline-block position-relative bg-gray py-2 px-3 rounded-5 text-12 -tracking-0/5 text-slate-500 text-uppercase">
-                        {ab.header}
-                      </h6>
-                    </div>
-                    <div className="ld-fancy-heading position-relative">
-                      <h2 className="ld-fh-element position-relative lqd-highlight-custom lqd-highlight-custom-2 mt-0/25em mb-0/75em text-38 is-in-view lqd-unit-animation-done">
-                        <mark className="lqd-highlight">
-                          <span className="lqd-highlight-txt">
-                            {ab.title.split(" ").at(0)}
-                          </span>
-                          <span
-                            className="bottom-10 left-0 lqd-highlight-inner bg-purple-100 style-TgRvE"
-                            id="style-TgRvE"
-                          >
-                            <svg
-                              className="lqd-highlight-brush-svg lqd-highlight-brush-svg-2"
-                              width={233}
-                              height={13}
-                              viewBox="0 0 233 13"
-                              xmlns="http://www.w3.org/2000/svg"
-                              aria-hidden="true"
-                              preserveAspectRatio="none"
-                            >
-                              <path
-                                d="m.624 9.414-.312-2.48C0 4.454.001 4.454.002 4.454l.035-.005.102-.013.398-.047c.351-.042.872-.102 1.557-.179 1.37-.152 3.401-.368 6.05-.622C13.44 3.081 21.212 2.42 31.13 1.804 50.966.572 79.394-.48 113.797.24c34.387.717 63.927 2.663 84.874 4.429a1048.61 1048.61 0 0 1 24.513 2.34 641.605 641.605 0 0 1 8.243.944l.432.054.149.02-.318 2.479-.319 2.48-.137-.018c-.094-.012-.234-.03-.421-.052a634.593 634.593 0 0 0-8.167-.936 1043.26 1043.26 0 0 0-24.395-2.329c-20.864-1.76-50.296-3.697-84.558-4.413-34.246-.714-62.535.332-82.253 1.556-9.859.612-17.574 1.269-22.82 1.772-2.622.251-4.627.464-5.973.614a213.493 213.493 0 0 0-1.901.22l-.094.01-.028.004Z"
-                                fill="#7150E8"
-                              />
-                            </svg>
-                          </span>
-                        </mark>
-                        {ab.title.split(" ").slice(1).join(" ")}
-                        &nbsp;🔔
-                      </h2>
-                    </div>
-                    <div className="mb-30 iconbox d-flex flex-grow-1 position-relative iconbox-circle iconbox-icon-ripple lqd-unit-animation-done">
-                      <div className="iconbox-icon-wrap me-2">
-                        <div className="iconbox-icon-container inline-flex position-relative z-1 rounded-full text-24 w-30 h-30 text-accent bg-white shadow-md">
-                          <i
-                            aria-hidden="true"
-                            className="fa fa-circle text-success"
-                          />
-                        </div>
-                      </div>
-                      <p className="text-13 leading-16 m-0 lqd-iconbox-heading">
-                        {ab.desc}
-                      </p>
+        <div className="col-lg-12 d-flex justify-content-center flex-column">
+          {aboutTypes.map((ab) => {
+            
+            return (
+              <>
+                <div className="d-flex flex-row">
+                  <div className="col-lg-6">
+                    <div className="w-100">
+                      <img
+                        src={`./images/${ab.img}`}
+                        style={{ opacity: ".8" }}
+                        className="shadow img-fluid"
+                      />
                     </div>
                   </div>
-                  <a href="#" className="text-dark">
-                    <span
-                      data-text="Learn More"
-                      className="btn-txt fw-bold  text-decoration-underline"
-                      style={{ textUnderlineOffset: "10px" }}
-                    >
-                      Learn More <i class="fa-solid fa-arrow-right"></i>
-                    </span>
-                  </a>
+                  <div className="col-lg-6 py-5 px-5 align-items-center text-start">
+                    <div className="d-flex flex-wrap ca-initvalues-applied lqd-animations-done solutions-inner">
+                      <div className="d-flex flex-wrap pb-200 module-content">
+                        <div className="m-0 mb-4 rounded-1 bg-slate-200 ld-fancy-heading position-relative animation-element lqd-unit-animation-done">
+                          <h6 className=" ld-fh-element d-inline-block position-relative bg-gray py-2 px-3 rounded-5 text-12 -tracking-0/5 text-slate-500 text-uppercase">
+                            {ab.header}
+                          </h6>
+                        </div>
+                        <div className="ld-fancy-heading position-relative mb-3">
+                          <h2 className="ld-fh-element position-relative lqd-highlight-custom lqd-highlight-custom-2 mt-0/25em mb-0/75em text-24 is-in-view lqd-unit-animation-done">
+                            <mark className="lqd-highlight">
+                              <span className="lqd-highlight-txt">
+                                {ab.title.split(" ").at(0)}
+                              </span>
+                              <span
+                                className="bottom-10 left-0 lqd-highlight-inner bg-purple-100 style-TgRvE"
+                                id="style-TgRvE"
+                              >
+                                <svg
+                                  className="lqd-highlight-brush-svg lqd-highlight-brush-svg-2"
+                                  width={233}
+                                  height={13}
+                                  viewBox="0 0 233 13"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  aria-hidden="true"
+                                  preserveAspectRatio="none"
+                                >
+                                  <path
+                                    d="m.624 9.414-.312-2.48C0 4.454.001 4.454.002 4.454l.035-.005.102-.013.398-.047c.351-.042.872-.102 1.557-.179 1.37-.152 3.401-.368 6.05-.622C13.44 3.081 21.212 2.42 31.13 1.804 50.966.572 79.394-.48 113.797.24c34.387.717 63.927 2.663 84.874 4.429a1048.61 1048.61 0 0 1 24.513 2.34 641.605 641.605 0 0 1 8.243.944l.432.054.149.02-.318 2.479-.319 2.48-.137-.018c-.094-.012-.234-.03-.421-.052a634.593 634.593 0 0 0-8.167-.936 1043.26 1043.26 0 0 0-24.395-2.329c-20.864-1.76-50.296-3.697-84.558-4.413-34.246-.714-62.535.332-82.253 1.556-9.859.612-17.574 1.269-22.82 1.772-2.622.251-4.627.464-5.973.614a213.493 213.493 0 0 0-1.901.22l-.094.01-.028.004Z"
+                                    fill="#7150E8"
+                                  />
+                                </svg>
+                              </span>
+                            </mark>
+                            {ab.title.split(" ").slice(1).join(" ")}
+                            &nbsp;🔔
+                          </h2>
+                        </div>
+                        <div className="mb-30 iconbox d-flex flex-grow-1 position-relative iconbox-circle iconbox-icon-ripple lqd-unit-animation-done">
+                          <div className="iconbox-icon-wrap me-2">
+                            <div className="iconbox-icon-container inline-flex position-relative z-1 rounded-full text-24 w-30 h-30 text-accent bg-white shadow-md">
+                              <i
+                                aria-hidden="true"
+                                className="fa fa-circle text-success fs-6"
+                              />
+                            </div>
+                          </div>
+                          <p className="text-13 leading-16 m-0 lqd-iconbox-heading">
+                            {ab.desc}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              );
-            })}
-          </div>
+              </>
+            );
+          })}
         </div>
       </div>
     </>
