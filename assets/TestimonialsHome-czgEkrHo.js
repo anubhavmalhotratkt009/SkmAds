a75618a6799b57932d0665192f50eee2967aa580
Anubhav <anubhav@tekiestech.com>
2024-02-19 22:06:25 +0530

    Updates

--- assets/TestimonialsHome-czgEkrHo.js
+++ assets/TestimonialsHome-czgEkrHo.js
@@ -1,4 +1,4 @@
-import{r as D,j as e}from"./index-MIRQLM35.js";import{S as X}from"./slick-theme-PZYg0AAb.js";/* empty css                     */import"./index-RyzN9TA-.js";var K={};function F(a){if(!a||typeof window>"u")return;const s=document.createElement("style");return s.setAttribute("type","text/css"),s.innerHTML=a,document.head.appendChild(s),a}Object.defineProperty(K,"__esModule",{value:!0});var t=D;function O(a){return a&&typeof a=="object"&&"default"in a?a:{default:a}}var l=O(t);F(`.rfm-marquee-container {
+import{r as D,j as e}from"./index-B3plne4g.js";import{S as X}from"./slick-theme-UIGB_q4b.js";/* empty css                     */import"./index-J2RtakEt.js";var K={};function F(a){if(!a||typeof window>"u")return;const s=document.createElement("style");return s.setAttribute("type","text/css"),s.innerHTML=a,document.head.appendChild(s),a}Object.defineProperty(K,"__esModule",{value:!0});var t=D;function O(a){return a&&typeof a=="object"&&"default"in a?a:{default:a}}var l=O(t);F(`.rfm-marquee-container {
   overflow-x: hidden;
   display: flex;
   flex-direction: row;
