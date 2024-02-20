396cbc7c251929a20e871ed177c58007730fe907
Anubhav <anubhav@tekiestech.com>
2024-02-20 13:33:32 +0530

    Updates

--- assets/ServicesHome-ue9yxt3r.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-7VXnCI1s.js";const s=()=>e.jsx(e.Fragment,{children:e.jsx("div",{})});export{s as default};
--- assets/TestimonialsHome-kP76EFmd.js
+++ assets/TestimonialsHome-kP76EFmd.js
@@ -1,4 +1,4 @@
-import{r as D,j as e}from"./index-7VXnCI1s.js";import{S as X}from"./slick-theme-LEdd4l9V.js";/* empty css                     */import"./index-WE5jyWgn.js";var K={};function F(a){if(!a||typeof window>"u")return;const s=document.createElement("style");return s.setAttribute("type","text/css"),s.innerHTML=a,document.head.appendChild(s),a}Object.defineProperty(K,"__esModule",{value:!0});var t=D;function O(a){return a&&typeof a=="object"&&"default"in a?a:{default:a}}var l=O(t);F(`.rfm-marquee-container {
+import{r as D,j as e}from"./index-nbDmOLT8.js";import{S as X}from"./slick-theme-X7ojryev.js";/* empty css                     */import"./index-gRs3_Fm4.js";var K={};function F(a){if(!a||typeof window>"u")return;const s=document.createElement("style");return s.setAttribute("type","text/css"),s.innerHTML=a,document.head.appendChild(s),a}Object.defineProperty(K,"__esModule",{value:!0});var t=D;function O(a){return a&&typeof a=="object"&&"default"in a?a:{default:a}}var l=O(t);F(`.rfm-marquee-container {
   overflow-x: hidden;
   display: flex;
   flex-direction: row;
